6d02cfc5185cdc8070cff439f335c98d6f72df19
asifmanan <asifmanan@gmail.com>
2025-09-06 12:14:33 +0100

    Addres redirect rule for /apply to redirect o /admissions/apply

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Redirect /apply to /admissions/apply
+  async redirects() {
+    return [
+      {
+        source: "/apply",
+        destination: "/admissions/apply",
+        permanent: true, // use false if you want it temporary
+      },
+    ]
+  },
 };
 
 export default nextConfig;
