e6ae393f8e50333b37af8b2fa1d167d614305128
asifmanan <asifmanan@gmail.com>
2025-09-06 12:16:12 +0100

    Adds google maps Embed on contact page.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -102,8 +102,16 @@ export default function ContactPage() {
 
             {/* Map Placeholder */}
             <div className="mt-8">
-              <div className="w-full h-64 bg-gray-200 rounded-xl flex items-center justify-center text-gray-500">
-                Google Maps Embed Placeholder
+              <div className="w-full h-64 overflow-hidden rounded-lg shadow-lg">
+                  <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d291.85593807324324!2d72.03921351564486!3d34.188361784024046!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38decb1638fd4e37%3A0x939c38bf49fb4203!2sAbdul%20Manan%20Memorial%20Institute%20of%20Medical%20Sciences!5e1!3m2!1sen!2suk!4v1757155842422!5m2!1sen!2suk"
+                    width="100%"
+                    height="100%"
+                    style={{ border: 0 }}
+                    allowFullScreen
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade">
+                  </iframe>
               </div>
             </div>
           </div>
